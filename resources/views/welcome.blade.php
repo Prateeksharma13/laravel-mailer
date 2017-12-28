01a8bb0bbb32987cf7d09a2ed190db40584bd914
prateek <emailprateeksharma@gmail.com>
2017-12-27 18:31:29 -0800

    setting up emailer

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Accept delete event </title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  </head>
+  <body style="  width:600px; margin: auto; position: relative; font-family: sans-serif; font-size: 14px; line-height: 1.42857143;
+  color: #333;background-color: #fff;">
+    <table class="emailer-bg" style="width:600px; margin:auto;">
+      <tr>
+        <td style="display:-webkit-box">
+            <img src="https://raw.githubusercontent.com/Prateeksharma13/images-repo/master/shri-page/images/Events1.jpg" class="emailer" style=" height: 35vh; width: 100%; background-size: cover;background-position: right bottom;
+            background-repeat: no-repeat;">
+          <div class="link" style=" margin-top: -42%;margin-right: 30px;font-size: 12px;margin-left: 77%;">
+              <a href="#" style="  color: #fff; text-decoration:none;">Go to Shrimad.com <i class="fa fa-caret-right" aria-hidden="true" style=" font-size: 10px;
+                margin-left: 5px;"></i></a>
+          </div>
+          <div class="white-logo" style="    text-align: center;width: 100%; margin-top: 60px;">
+          <img src="https://raw.githubusercontent.com/Prateeksharma13/images-repo/master/shri-page/images/shrimad-white-logo.png  " style="width: 176px;">
+          <h1 style=" text-align: center; width: 100%;  opacity: 0.63;  font-family: Montserrat-Bold;
+          font-size: 26px; color: #FFFFFF; letter-spacing: 0; line-height: 36px;   margin-top: 0vh; margin-left: -1%;">Event Invitation</h1>
+          </div>
+        </td>
+        </tr>
+        </table>
+        <table class="content" style="width:600px; margin:auto; margin-bottom:60px;">
+          <tr class="left-con" style="width:100%;">
+            <td class="left" style="width:40%; vertical-align: top; padding-top: 30px;">
+                <p style="font-size:22px; font-family: Montserrat-Bold; color:#4A4A4A; margin: 0 0 10px;">(event name)Aenean lacinia bibendum<br>nulla sed consectetur</p>
+                <div class= "left-btn" style="padding-top:20px;">
+                <button style="width: 160px; background-color: #4A90E2; border: none; border-radius: 4px; height: 40px;margin-top: 20px;
+                ;font-size: 16px;  color: #FFFFFF;line-height: 24px; font-family: Montserrat-Bold;">Accept invite</button>
+                <button style="width: 160px;  background-color: #4A90E2; border: none; border-radius: 4px; height: 40px;margin-top: 20px;
+               font-size: 16px;  color: #FFFFFF; line-height: 24px; font-family: Montserrat-Bold;">Reject invite</button>
+                </div>
+            </td>
+            <td class="right" style="width:60%;">
+                <p style="font-size:15px; padding-top:35px; margin: 0 0 10px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <div class="venue" style="margin-top:20px">
+                  <h6 style="font-size:14px; font-family: Montserrat-Bold; font-weight:700; margin-top: 10px;
+                  margin-bottom:10px">Venue</h6>
+                  <p style="font-size:15px;">Excepteur sint occaecat cupidatat non proident</p>
+                </div>
+                <table class="row schedule" style="width: 100%; margin: 0px; margin-top:20px;">
+                    <tr>
+                      <th style="font-size:14px; font-family: Montserrat-Bold; font-weight:700; text-align: left;padding: 0;">Date</th>
+                      <th style="font-size:14px; font-family: Montserrat-Bold; font-weight:700; text-align: left;padding: 0;" >Start Time</th>
+                      <th style="font-size:14px; font-family: Montserrat-Bold; font-weight:700; text-align: left;padding: 0;">End Time</th>
+                    </tr>
+                    <tr>
+                      <td style="font-size:15px;">25 September 2018</td>
+                      <td style="font-size:15px;">9:30AM</td>
+                      <td style="font-size:15px;">11:30AM</td>
+                    </tr>
+                  </table>
+            </td>
+          </tr>
+        </table>
+        <table style="width:600px; margin:auto;">
+          <tr class="footer-bg" style="width: 100%;height: 30vh;">
+            <td style=" background-color:#F5F5F5;">
+                <div class="footer-logo" style="width: 100%; text-align: center;">
+                    <img src="https://raw.githubusercontent.com/Prateeksharma13/images-repo/master/shri-page/images/Logo.png" style=" width: 100px; margin-top: 10vh;margin-bottom: 20px;">
+                    </div>
+                    <div class="border-line" style="border-bottom: 3px solid #fff;">
+                    </div>
+                    <!-- <div class="social-icons" style="width: 100%; text-align: center; margin-top: 15px;">
+                            <i class="fa fa-facebook" aria-hidden="true" style=" margin-left: 13px; margin-right: 15px;"></i>
+                            <i class="fa fa-twitter" aria-hidden="true" style="margin-right: 15px;"></i>
+                            <i class="fa fa-google-plus" aria-hidden="true" style="margin-right: 15px;"></i>
+                            <i class="fa fa-linkedin" aria-hidden="true" style="margin-right: 15px;"></i>
+                    </div> -->
+                    <div class="copy-right" style="   width: 50%;display: inline; margin-left:30px;">
+                           <p style="font-family: 'Montserrat', sans-serif; font-size: 12px;  color: #757575;  letter-spacing: 0.26px;
+                           line-height: 22px;margin-top: 30px;  display: inline-flex;"> © Shrimad 2018, All rights Reserved</p>
+                    </div>
+                    <div class="contact-link" style=" float: right; margin: 30px 30px;">
+                        <a href="#" style="font-family: 'Montserrat', sans-serif;font-size: 12px; color: #E76450;    letter-spacing: 0.25px;
+                        line-height: 22px; text-decoration: none;">Contact</a>
+                    </div>
+            </td>
+          </tr>
+        </table>
+  </body>
+</html>
